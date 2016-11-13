4ce3d551e6460d96fc16684aa90fc7d3a99c9cf6
Tiago Lisboa <tiago.caio.ol@gmail.com>
2016-11-13 16:32:01 -0300

    box to colide

--- js/main.js
+++ js/main.js
@@ -47,15 +47,37 @@
         }
 
         this.update = function (paddle) {
-            if (this.y+this.dy > canvas.height-paddle.h-this.r &&
+            if (this.y+this.dy == canvas.height-paddle.h-this.r &&
                 this.x+this.dx > paddle.x-this.r &&
                 this.x+this.dx < paddle.x+paddle.w+this.r) {
 
                 this.dy = -this.dy;
             }
+            var x = this.x + this.dx, y = this.y + this.dy, r = this.r;
+
+            var hitBox = boxes.filter(function (index) {
+                return x > index.x-r &&
+                x < index.x+index.w+r &&
+                y > index.y-r &&
+                y < index.y+index.h+r;
+            });
+
+            boxes = boxes.filter(function (index) {
+                return !(x > index.x-r &&
+                x < index.x+index.w+r &&
+                y > index.y-r &&
+                y < index.y+index.h+r);
+            });
+
+            console.log(boxes.length);
+
+            if (hitBox.length>0) {
+                this.dy = -this.dy;
+                this.dx = -this.dx;
+            }
 
             if (this.y + this.dy > canvas.height-this.r) {
-                clearInterval(gameLoop);
+                clearInterval(game.loop);
             }
 
             if (this.y + this.dy < this.r ||
@@ -99,22 +121,55 @@
         }
     }
 
+    // Blocks constructor
+    function Box (x, y, _ID){
+        this.x = x;
+        this.y = y;
+        this.w = 78;
+        this.h = 13;
+        this._ID = _ID;
+
+        this.draw = function () {
+            ctx.beginPath();
+            ctx.rect(this.x, this.y, this.w, this.h);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+
     // Initing objects
     var ball = new Ball (canvas.width/2, canvas.height-30, 10);
     var paddle = new Paddle (7);
+    var boxes = [];
 
+    var x = 0;
+    for (var i = 0; i < 6; i ++) {
+        for (var j = 0; j < 5; j ++){
+            boxes[x] = new Box(i*80, j*15, x);
+            x++;
+        }
+    }
     // Game Loop
 
+    game.init = function () {
+
+    }
     game.draw = function (){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < boxes.length; i++) {
+            boxes[i].draw();
+        }
         ball.draw();
         paddle.draw();
     }
     game.update = function (){
         paddle.update();
-        ball.update(paddle);
+        ball.update(paddle, boxes);
     }
-    var gameLoop = setInterval(function () {
+    game.init();
+
+    game.loop = setInterval(function () {
         game.draw();
         game.update();
     }, game.v);
